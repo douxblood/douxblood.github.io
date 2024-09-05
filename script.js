31aaf35fd610e3fe84113048b2bae95849a7572a
douxblood <jisnetsant@gmail.com>
2024-09-05 08:52:50 -0300

    menu icon con js

---
+++ script.js
@@ -0,0 +1,9 @@
+//dar clic en icono de menu y desplegar .links
+
+const menu = document.querySelector("menu");
+const nav = document.querySelector(".links"); 
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
